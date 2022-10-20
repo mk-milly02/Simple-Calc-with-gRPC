3e76f6c812cdfe3e540945f24ffc7d116fc79c74
Jonas Ababio <jonasagesmilly@outlook.com>
2022-10-20 21:59:15 +0000

    final commit

--- gRPC.Client/Program.cs
+++ gRPC.Client/Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using gRPC.Server;
+using Grpc.Net.Client;
+
+var channel = GrpcChannel.ForAddress("https://localhost:7180");
+var client = new Calculator.CalculatorClient(channel);
+
+Console.WriteLine("Welcome to Simple Calc");
+
+CalculatorRequest request = new();
+
+Console.Write("\nInput the first number: ");
+request.X = Console.ReadLine();
+
+Console.Write("\nInput the second number: ");
+request.Y = Console.ReadLine();
+
+var sum = client.Add(request);
+var difference = client.Subtract(request);
+var product = client.Multiply(request);
+var quotient = client.Divide(request);
+
+Console.WriteLine("\n");
+
+Console.WriteLine($"Sum: {sum.Result}");
+Console.WriteLine($"Difference: {difference.Result}");
+Console.WriteLine($"Product: {product.Result}");
+Console.WriteLine($"Quotient: {quotient.Result}");
+
+Console.ReadKey();
+
--- gRPC.Server/Services/CalculatorService.cs
+++ gRPC.Server/Services/CalculatorService.cs
@@ -8,21 +8,30 @@ public class CalculatorService : Calculator.CalculatorBase
 
     public override Task<CalculatorReply> Add(CalculatorRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new CalculatorReply { Result = request.X + request.Y });
+        return !double.TryParse(request.X, out double x) || !double.TryParse(request.Y, out double y)
+            ? throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid input"))
+            : Task.FromResult(new CalculatorReply { Result = (x + y).ToString() });
+
     }
 
     public override Task<CalculatorReply> Subtract(CalculatorRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new CalculatorReply { Result = request.X - request.Y });
+        return !double.TryParse(request.X, out double x) || !double.TryParse(request.Y, out double y)
+            ? throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid input"))
+            : Task.FromResult(new CalculatorReply { Result = (x - y).ToString() });
     }
 
     public override Task<CalculatorReply> Multiply(CalculatorRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new CalculatorReply { Result = request.X * request.Y });
+        return !double.TryParse(request.X, out double x) || !double.TryParse(request.Y, out double y)
+            ? throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid input"))
+            : Task.FromResult(new CalculatorReply { Result = (x * y).ToString() });
     }
 
     public override Task<CalculatorReply> Divide(CalculatorRequest request, ServerCallContext context)
     {
-        return Task.FromResult(new CalculatorReply { Result = request.X / request.Y }); ;
+        return !double.TryParse(request.X, out double x) || !double.TryParse(request.Y, out double y)
+            ? throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid input"))
+            : Task.FromResult(new CalculatorReply { Result = (x / y).ToString() });
     }
 }
